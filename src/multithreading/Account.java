e004ce0028ff6f7f5b5a340098f9167af1d8cf3a
Anil <anildiwakar956@gmail.com>
2026-01-25 23:21:39 +0530

    multithreading notes and programs

--- src/multithreading/Account.java
+++ src/multithreading/Account.java
@@ -1,11 +1,30 @@
 package multithreading;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Account {
 	public int balence = 1000;
-	public void withdraw(int amount)
+	//synchronized method
+	/*public synchronized void withdraw(int amount)
+	{
+		  balence = balence - amount;
+	}*/
+	//synchronized block
+	/*public void withdraw(int amount)
 	{
 		synchronized(this) {
 		  balence = balence - amount;
+		}
+	}*/
+	Lock lock=new ReentrantLock();
+	public void withdraw(int amount)
+	{
+		try {
+		  lock.lock();
+		  balence = balence - amount;
+		} finally {
+		  lock.unlock();
 		}
 	}
 
